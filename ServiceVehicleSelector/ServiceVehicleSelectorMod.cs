63f6bee6a84d14019739e53145ac8ec4cade4b69
Bloody Penguin <bloodypenguin.skylines@gmail.com>
2021-04-24 17:25:21 +0200

    Bump version to 5.1.0

--- ServiceVehicleSelector/ServiceVehicleSelectorMod.cs
+++ ServiceVehicleSelector/ServiceVehicleSelectorMod.cs
@@ -17,7 +17,7 @@ namespace ServiceVehicleSelector2
 {
     public class ServiceVehicleSelectorMod : IUserMod, ILoadingExtension
     {
-        private static readonly string _version = "5.0.1";
+        private static readonly string _version = "5.1.0";
         private static readonly string _dataID = "CTS_BuildingData";
         private static readonly string _dataVersion = "v001";
         public static Dictionary<ushort, HashSet<string>> BuildingData;
