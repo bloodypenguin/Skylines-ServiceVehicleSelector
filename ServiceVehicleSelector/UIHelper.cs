0acc502c5aece3d990f09842deb20dc324a5474f
Bloody Penguin <bloodypenguin.skylines@gmail.com>
2022-02-06 22:13:16 +0100

    Use checkboxes instead of the standard vehicle selector

--- ServiceVehicleSelector/UIHelper.cs
+++ ServiceVehicleSelector/UIHelper.cs
@@ -23,6 +23,39 @@ namespace ServiceVehicleSelector2
         return UIHelper._font;
       }
     }
+    
+    public static UICheckBox CreateCheckBox(UIComponent parent,
+      string name,
+      string text,
+      bool state) {
+      UICheckBox checkBox = parent.AddUIComponent<UICheckBox>();
+      checkBox.name = name;
+
+      checkBox.height = 16f;
+      checkBox.width = parent.width - 10f;
+
+      UISprite uncheckedSprite = checkBox.AddUIComponent<UISprite>();
+      uncheckedSprite.spriteName = "check-unchecked";
+      uncheckedSprite.size = new Vector2(16f, 16f);
+      uncheckedSprite.relativePosition = Vector3.zero;
+
+      UISprite checkedSprite = checkBox.AddUIComponent<UISprite>();
+      checkedSprite.spriteName = "check-checked";
+      checkedSprite.size = new Vector2(16f, 16f);
+      checkedSprite.relativePosition = Vector3.zero;
+      checkBox.checkedBoxObject = checkedSprite;
+
+      checkBox.label = checkBox.AddUIComponent<UILabel>();
+      checkBox.label.text = text;
+      checkBox.label.autoSize = false;
+      checkBox.label.height = 20f;
+      checkBox.label.verticalAlignment = UIVerticalAlignment.Middle;
+      checkBox.label.relativePosition = new Vector3(20f, 0f);
+
+      checkBox.isChecked = state;
+
+      return checkBox;
+    }
 
     public static bool IsFullyClippedFromParent(UIComponent component)
     {
